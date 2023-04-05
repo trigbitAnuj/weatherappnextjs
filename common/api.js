9b3fdb609d85e9b1f26a19fc42bf5fd8a73422d9
trigbitAnuj <trigbit.anuj@gmail.com>
2023-04-05 09:40:03 +0530

    add try catch functionality to fetch  api

--- common/api.js
+++ common/api.js
@@ -1,8 +1,12 @@
 export const  getCurrentWeatherData = async (city) => {
    
-  
-    let response = await fetch(
-     `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.NEXT_PUBLIC_API_KEY}&units=metric  `
-     );
+  try{
+    let response = await fetch(`api/weather?city=${city}`)
     return response.json();
+}
+catch(error){
+  console.log(error);
+
+}
+   
   };
